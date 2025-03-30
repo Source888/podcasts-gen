aaaffc2e2eafaa1d2e4ef73b43557d0f15b9420f
Source888 <constantinsolovyov@gmail.com>
2025-03-30 23:49:06 +0000

    Update to entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 echo "========================"
 
-git config --global user.name "${GITHAB_ACTOR}"
+git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /user/bin/feed/.py
+python3 /user/bin/feed.py
 
 git add -A && git commit -m "Update feed"
 git push --set-upstream origin main 
